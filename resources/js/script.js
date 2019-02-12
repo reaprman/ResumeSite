19b306c601cc0150a5efa9f0fd72ce27d3df9016
ReaprMan <ryan.logan01@gmail.com>
2019-02-12 01:16:56 -0600

    fix: js alteration for carousel

--- resources/js/script.js
+++ resources/js/script.js
@@ -55,9 +55,9 @@ function modalToggle(imgPath){
 function fillModalImg(imgPath){
     //let imgArr = imgPath.split(".");
     let imgName = imgPath.slice(0,-1);
-    let img1 = `${imgName}1.${imgArr[3]}`;
-    let img2 = `${imgName}2.${imgArr[3]}`;
-    let img3 = `${imgName}3.${imgArr[3]}`;
+    let img1 = `${imgName}1`; //`${imgName}1.${imgArr[3]}`
+    let img2 = `${imgName}2`; 
+    let img3 = `${imgName}3`;
 
     const imgModal1 = document.getElementById('mPic1');
     const imgModal2 = document.getElementById('mPic2');
